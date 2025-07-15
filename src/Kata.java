da680d385671583d87153118b289f3cded028a43
tbaharvand <tarabaharvand97@gmail.com>
2025-07-15 22:57:36 +0330

    clear unnecessary comments related to issue  #1

--- src/Kata.java
+++ src/Kata.java
@@ -21,18 +21,3 @@ public class Kata {
         nameInStr("ABCD", "BD");
     }
 }
-//sub = str.replaceAll("[^"+name.charAt(s)+"]" , "");
-//System.out.print(sub);
-
-   /* String sub = "";
-        for (int i = 0; i < name.length(); i++) {
-        for (int j = 0; j < str.length(); j++) {
-        if (str.charAt(j) == name.charAt(i)) {
-        sub = String.valueOf(str.charAt(j));
-        System.out.print(sub);
-        }
-
-        }
-        }
-        return sub.contains(name);
-        }*/
